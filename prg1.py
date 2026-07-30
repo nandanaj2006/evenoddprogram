45a2349db6bb0ec9e9b48cd123b9970d568f6083
nandanaj2006 <nandanaj2103@gmail.com>
2026-07-30 09:22:45 +0530

    Create prg1.py

---
+++ prg1.py
@@ -0,0 +1,10 @@
+print("Enter 3 numbers ")
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+if a>=b and a>=c:
+  print(a,"is greater")
+elif b>=c and b>=a:
+  print(b, "is greater ")
+else:
+  print(c , "c is greater")
