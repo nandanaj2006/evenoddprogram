a4d574480f0724a792001ad56c5537959be39fe8
nandanaj2006 <nandanaj2103@gmail.com>
2026-07-30 12:35:02 +0530

    created program

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+import sys
+num = int(sys.argv[1]("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reversed number is", reverse)
